5fb103d760ca9e71641cd3e9e8d938c98ffb126f
Seonghyeon Cho <seonghyeoncho96@gmail.com>
2023-05-02 11:28:43 +0900

    Remove basic assertion test

--- main_test.cc
+++ main_test.cc
@@ -20,11 +20,6 @@ protected:
 };
 
 /// Tests
-
-TEST(HelloTest, BasicAssertions) {
-	EXPECT_EQ(7 * 6, 42);
-}
-
 TEST_F(GeoIPLoaderFixture, NormalMccmnc) {
 	// 1. Exists in ISP DB range
 	// 1-1. mcc, mnc exist
